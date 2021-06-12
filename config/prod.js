b8ae74eb547436258c6fe82decaf70eb58e13e0b
Salekin Imran <abir045@gmail.com>
2021-06-12 21:03:35 +0600

    updated with heroku config var

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 //this file is used for production
 module.exports = {
-  MongoDB:
-    "mongodb+srv://LimoTours:48664842@limotours.jrmdy.mongodb.net/Limo?retryWrites=true&w=majority",
+  MongoDB: process.env.MONGODB_URI,
 };
---
+++ models/car.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  brand: {
+    type: String,
+  },
+  model: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+  type: {
+    type: String,
+  },
+  priceperWeek: {
+    type: Number,
+  },
+  pricePerHour: {
+    type: Number,
+  },
+  location: [
+    {
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      lat: {
+        type: Number,
+      },
+      lng: {
+        type: Number,
+      },
+    },
+  ],
+  date: {
+    type: date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Car", carSchema);
